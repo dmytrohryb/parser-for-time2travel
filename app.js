e31309f3e5bfa5ab1d502c4599be13081c2ca04c
dmytro hryb <dmytro.hryb@nure.ua>
2020-07-21 20:01:59 +0300

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Parser = require('./src/parser')
 const express = require('express')
 const sqlite3 = require('sqlite3').verbose();
 
+const app = express();
+
 let db = new sqlite3.Database('./data', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
@@ -9,18 +11,31 @@ let db = new sqlite3.Database('./data', sqlite3.OPEN_READWRITE, (err) => {
     console.log('Connected to database.');
 });
 
-Parser()
-    .then(res => {
-        for(let i = 0; i < res.length; i++){
-            db.run('insert into tours(title, date, duration, link, location, price)values(?,?,?,?,?,?)', [res[i].title, res[i].date, res[i].duration, res[i].link, res[i].location, res[i].price], function (err){
-                    if(err) {
-                        return console.error(err.message)
-                    }
-                console.log(`A row has been inserted with rowid ${this.lastID}`)
-                })
+setInterval(getData, 10000)
+
+async function getData (){
+    let res = await Parser()
+    db.run('delete from tours', function (err){
+        if(err){
+            return console.error(err.message)
         }
     })
-    .catch(err => {
-        console.error(err.message)
-    })
+    for(let i = 0; i < res.length; i++){
+        db.run('insert into tours(title, date, duration, link, location, price)values(?,?,?,?,?,?)',
+            [res[i].title, res[i].date, res[i].duration, res[i].link, res[i].location, res[i].price],
+            function (err){
+            if(err) {
+                return console.error(err.message)
+            }
+        })
+    }
+}
 
+app.listen(3000);
+
+app.get("/", function(request, response){
+    db.all('SELECT * from tours', (err, rows ) => {
+        console.log('test')
+        response.send(rows);
+    });
+});
--- src/sites/Pohodvgory.js
+++ src/sites/Pohodvgory.js
@@ -1,23 +1,23 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-const URL = 'https://pohod-v-gory.com/tours/%d1%80%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b5-%d0%bf%d0%be%d1%85%d0%be%d0%b4%d0%be%d0%b2/'
+const URL = 'https://pohod-v-gory.com'
+const SHEDULE = '/tours/%d1%80%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b5-%d0%bf%d0%be%d1%85%d0%be%d0%b4%d0%be%d0%b2/'
 
 let getDataPohodvgory = async function(cur){
     const tempData = []
-    const response = await axios.get(URL)
+    const response = await axios.get(URL + SHEDULE)
     const $ = cheerio.load(response.data)
     $('div.date-pohods-wrapper div.date_pohods div.schd-box').each((i, elem) => {
         tempData.push({
             date: $(elem).find('div.schd-tbl div.start-date').text().substr(1, 10),
             title: $(elem).find('div.schd-name a').text().replace(/\r?\n/g, ""),
-            link: $(elem).find('div.schd-name a').attr('href'),
+            link: URL + $(elem).find('div.schd-name a').attr('href'),
             price: getPriceOnlyNum($(elem).find('div.schd-price').text().replace(/\s/g, ''), cur),
             location: $(elem).find('div.schd-place').text().replace(/\s/g, ''),
             duration: getDurationOnlyNum($(elem).find('div.schd-loc').text().replace(/\s/g, ''))
         })
     })
-    console.log(tempData)
     return tempData
 }
 
