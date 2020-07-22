04d8388f556eb1f55164685ddd4b42b4066f69b2
dmytro hryb <dmytro.hryb@nure.ua>
2020-07-22 17:13:38 +0300

    tqwtwqtsa

--- index.js
+++ index.js
@@ -43,8 +43,13 @@ app.listen(port, () => {
     console.log(port)
 });
 
-app.post("/", function(request, response){
-    console.log(request.body)
+app.get("/", function (request, response) {
+    db.all(`select * from tours`, (err, rows ) => {
+        response.send(rows);
+    });
+})
+
+/*app.get("/", function(request, response){
     if(request.body.date && request.body.duration && request.body.cost){
         db.all(`SELECT * from tours WHERE date = ${request.body.date} AND duration = ${request.body.duration} AND price > ${request.body.cost.min} AND price < ${request.body.cost.max} LIMIT 15 OFFSET`, (err, rows ) => {
             console.log('test')
@@ -52,4 +57,4 @@ app.post("/", function(request, response){
         });
     }
     console.log('end post query')
-});
+});*/
