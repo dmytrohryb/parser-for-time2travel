3963abdc1700cda5a8a31b4306c9796671242c6f
dmytro hryb <dmytro.hryb@nure.ua>
2020-07-25 20:15:03 +0300

    ___

--- src/sites/Kuluarpohod.js
+++ src/sites/Kuluarpohod.js
@@ -56,6 +56,7 @@ async function getDataKuluarpohod(){
     for (let i = 0; i < routes.length; i++){
         result.push({
             date: durationsAndDate[i].date,
+            site: 'https://kuluarpohod.com',
             title: routes[i].title,
             link: routes[i].link,
             price: routes[i].price,
